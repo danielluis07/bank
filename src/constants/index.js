ddf519cab7e50a7b38cea740bede2dd4018ab844
danielluis07 <bombdaniel@hotmail.com>
2023-04-17 12:51:20 -0300

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,212 @@
+import {
+  people01,
+  people02,
+  people03,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  airbnb,
+  binance,
+  coinbase,
+  dropbox,
+  send,
+  shield,
+  star,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Início",
+  },
+  {
+    id: "features",
+    title: "Destaques",
+  },
+  {
+    id: "product",
+    title: "Produtos",
+  },
+  {
+    id: "clients",
+    title: "Clientes",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Recompensas",
+    content:
+      "Purus sem ac elementum facilisis ad curae malesuada aptent penatibus ex elit",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Seguro",
+    content:
+      "Purus sem ac elementum facilisis ad curae malesuada aptent penatibus ex elit",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Balanço de Transferências",
+    content:
+      "Purus sem ac elementum facilisis ad curae malesuada aptent penatibus ex elit",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Ipsum suscipit fermentum sociosqu facilisis etiam. Venenatis netus nam fringilla pretium aptent magna.",
+    name: "Ana",
+    title: "",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Ipsum suscipit fermentum sociosqu facilisis etiam. Venenatis netus nam fringilla pretium aptent magna.",
+    name: "João",
+    title: "",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Ipsum suscipit fermentum sociosqu facilisis etiam. Venenatis netus nam fringilla pretium aptent magna.",
+    name: "Cláudio",
+    title: "",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Usuários Ativos",
+    value: "3800+",
+  },
+  {
+    id: "stats-2",
+    title: "Confiáveis",
+    value: "230+",
+  },
+  {
+    id: "stats-3",
+    title: "Transações",
+    value: "$230M+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Conteúdo",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "Como funciona",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Criar",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explorar",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Termos & Serviços",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Comunidade",
+    links: [
+      {
+        name: "Central de ajuda",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Parceiros",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Sugestões",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Parceria",
+    links: [
+      {
+        name: "Nossos Sócios",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Seja um Sócio",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
